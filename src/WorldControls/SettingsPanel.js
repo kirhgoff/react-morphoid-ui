0fc259510defa165e8f49eb086c93f0aecc1e65e
Kirill Lastovirya <kirill.lastovirya@jobseeker.com.au>
2019-07-22 11:14:56 +1000

    Implemented sliders for multiple options

--- src/WorldControls/SettingsPanel.js
+++ src/WorldControls/SettingsPanel.js
@@ -5,35 +5,74 @@ function updateSettings(settings) {
         method: 'post',
         body: JSON.stringify(settings)
     })
-    .catch(error => console.log('Error: >>>', error));
+    .catch(error => console.log('Error: >>>', error))
+    .finally(() => console.log("updated settings..."));
 }
 
 function getSettings(handler) {
     fetch('/world/settings/get')
     .then(response => response.json())
     .then(payload => handler(payload))
-    .catch(error => console.log('Error: >>>', error));
+    .catch(error => console.log('Error: >>>', error))
+    .finally(() => console.log("got settings..."));
+}
+
+const SLIDERS = [
+    ["reproduce_cost", {min: -100, max: 0, desc: "Reproduce cost"}],
+    ["reproduce_threshold", {min: 0, max: 100, desc: "Reproduce threshold"}],
+];
+
+function SettingValueSlider({ settings, setSettings, slider_name, slider_info }) {
+    const key = "slider_" + slider_name;
+    return (
+        <div id={key} className="slider">
+            <div>{slider_info.desc}</div>
+            <input
+                   type="range"
+                   min={slider_info.min}
+                   max={slider_info.max}
+                   step="1"
+                   value={settings[slider_name]}
+                   onChange={event => {
+                       const value = event.target.value;
+                       setSettings(settings => {
+                           return { ...settings, [slider_name]: value };
+                       })
+                   }}
+                   //  onChange={event => {
+                   //      settings[slider_name] = event.target.value;
+                   //      setSettings(prev => {
+                   //          settings
+                   //      })
+                   //  }}
+
+            />
+            <div>{settings[slider_name]}</div>
+        </div>
+    );
 }
 
 export default function SettingsPanel() {
-    const [settings, setSettings] = useState({reproduce_threshold: 0});
+    const [settings, setSettings] = useState(null); // TODO: can I use null?
     useEffect(() => { getSettings(setSettings) },[]);
-    return (
-        <div>
-            <div onClick={() => {updateSettings(settings)}} className="bordered">Update settings</div>
-            <div className="slider-group">
-                <div id="slider_reproduce_threshold" className="slider">
-                    <div>Reproduce threshold:</div>
-                    <input type="range"
-                           min="0"
-                           max="200"
-                           step="1"
-                           value={settings.reproduce_threshold}
-                           onChange={event => { setSettings({ reproduce_threshold: event.target.value })}}
-                    />
-                    <div>{settings.reproduce_threshold}</div>
+
+    if (settings) {
+        console.log("settings: ", settings);
+        return (
+            <div>
+                <div onClick={() => {
+                    updateSettings(settings)
+                }} className="bordered">Update settings
+                </div>
+                <div className="slider-group">
+                    {SLIDERS.map((info) => {
+                        return <SettingValueSlider key={info[0]} {...{ settings, setSettings, slider_name: info[0], slider_info: info[1] }} />
+                    })}
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        console.log("not yet: ");
+        return (<div>Loading...</div>);
+    }
 }
